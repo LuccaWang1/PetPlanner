afa079531f19f3f37fce945cf7688e244aeddd3f
Lucca Wang <luccawang@gmail.com>
2024-01-24 17:10:53 -0700

    Added modals for 3 Features: Add A Pet, Add An Event, Add A Specialist.

--- static/js/add.jsx
+++ static/js/add.jsx
@@ -1,25 +1,20 @@
-'use strict';
+"use strict";
 
-function addPetModal(props) {
-  const [show, setShow] = React.useState(false);
+console.log("jsx is running");
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+// Import necessary components
+const { Button, Modal, Form } = ReactBootstrap;
 
+// START add a pet feature modal
+function AddPetModal(props) {
   return (
     <>
-      <Button variant="primary" onClick={handleShow}>
-        Launch demo modal
-      </Button>
-
       <Modal
-      {...props}
-      size="lg"
-      show={show}
-      onHide={handleClose}
-      backdrop="static"
-      keyboard={false}
-      centered
+        {...props}
+        size="lg"
+        backdrop="static"
+        keyboard={false}
+        centered
       >
         <Modal.Header closeButton>
           <Modal.Title>Modal heading</Modal.Title>
@@ -44,10 +39,10 @@ function addPetModal(props) {
           </Form>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
+          <Button variant="secondary" onClick={props.onHide}>
             Close
           </Button>
-          <Button variant="primary" onClick={handleClose}>
+          <Button variant="primary" onClick={props.onHide}>
             Save Changes
           </Button>
         </Modal.Footer>
@@ -56,24 +51,164 @@ function addPetModal(props) {
   );
 }
 
-function addAPet() {
-    const [modalShow, setModalShow] = React.useState(false);
-  
-    return (
-      <>
-        <Button variant="primary" onClick={() => setModalShow(true)}>
-          Launch vertically centered modal
-        </Button>
-  
-        <addPetModal
-          show={modalShow}
-          onHide={() => setModalShow(false)}
-        />
-      </>
-    );
-  }
+function AddAPet() {
+  console.log("Add pet");
 
-// Import necessary components
-const { Button, Modal, Form } = ReactBootstrap;  
+  const [modalShow, setModalShow] = React.useState(false);
 
-ReactDOM.render(<App />, document.querySelector('#add_pet'));
+  return (
+    <React.Fragment>
+      <Button variant="primary" onClick={() => setModalShow(true)}>
+        Pet
+      </Button>
+
+      <AddPetModal show={modalShow} onHide={() => setModalShow(false)} />
+    </React.Fragment>
+  );
+}
+
+const add_pet_div = document.querySelector("#add_pet")
+console.log(add_pet_div)
+
+ReactDOM.render(<AddAPet />, add_pet_div);
+// END add a pet feature modal
+
+
+
+// START add an event feature modal
+function AddEventModal(props) {
+  return (
+    <>
+      <Modal
+        {...props}
+        size="lg"
+        backdrop="static"
+        keyboard={false}
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Event Name</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="name@example.com"
+                autoFocus
+              />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label>Event title</Form.Label>
+              <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={props.onHide}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={props.onHide}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+function AddAnEvent() {
+  console.log("Add an event");
+
+  const [modalShow, setModalShow] = React.useState(false);
+
+  return (
+    <React.Fragment>
+      <Button variant="primary" onClick={() => setModalShow(true)}>
+        Event
+      </Button>
+
+      <AddEventModal show={modalShow} onHide={() => setModalShow(false)} />
+    </React.Fragment>
+  );
+}
+
+const add_event_div = document.querySelector("#add_event")
+console.log(add_event_div)
+
+ReactDOM.render(<AddAnEvent />, add_event_div);
+// END add an event feature modal
+
+
+
+// START add a specialist feature modal
+function AddSpecialistModal(props) {
+  return (
+    <>
+      <Modal
+        {...props}
+        size="lg"
+        backdrop="static"
+        keyboard={false}
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Specialist Name</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="name@example.com"
+                autoFocus
+              />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label>Specialist title</Form.Label>
+              <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={props.onHide}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={props.onHide}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+function AddASpecialist() {
+  console.log("Add a specialist");
+
+  const [modalShow, setModalShow] = React.useState(false);
+
+  return (
+    <React.Fragment>
+      <Button variant="primary" onClick={() => setModalShow(true)}>
+        Specialist
+      </Button>
+
+      <AddSpecialistModal show={modalShow} onHide={() => setModalShow(false)} />
+    </React.Fragment>
+  );
+}
+
+const add_specialist_div = document.querySelector("#add_specialist")
+console.log(add_specialist_div)
+
+ReactDOM.render(<AddASpecialist />, add_specialist_div);
+// END add a specialist feature modal
