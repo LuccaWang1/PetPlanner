e7af8042d9eb7bcf8b070212931fdc5f2c586926
Lucca Wang <luccawang@gmail.com>
2024-02-09 19:19:38 -0700

    Style updates, priorities in css, adding to seed

--- seed.py
+++ seed.py
@@ -26,8 +26,8 @@ test_user = Owner(
 model.db.session.add(test_user)
 model.db.session.commit()
 
-pet = Pet(
-    imgUrl="",
+pet1 = Pet(
+    imgUrl="/static/images/Feefee_hiking.png",
     species="dog",
     pet_fname="Feefee",
     pet_lname="Wang",
@@ -42,43 +42,82 @@ pet = Pet(
     emer_contact_phone="",
     emer_contact_email="test@testemailemer.com",
     insurance_company="Nationwide",
-    insurance_policy_num="",
-    pet_comment="I absolutely love this dog!",
+    insurance_policy_num="JYE896",
+    pet_comment="Loves tennis balls",
 )
 
-pet.owners.append(test_user)
+pet1.owners.append(test_user)
 
-event = Event(
+pet2 = Pet(
+    imgUrl="/static/images/petplanner-Bentley.jpg",
+    species="dog",
+    pet_fname="Bentley",
+    pet_lname="",
+    breed="Mutt/Mix",
+    birthday="2015-01-01",
+    age=8,
+    weight="20",
+    energy_level="medium",
+    coat="long",
+    emer_contact_fname="",
+    emer_contact_lname="",
+    emer_contact_phone="",
+    emer_contact_email="test@testemailemer.com",
+    insurance_company="Nationwide",
+    insurance_policy_num="PTN573",
+    pet_comment="Will lick your face",
+)
+
+pet2.owners.append(test_user)
+
+event1 = Event(
     title="Feefee's nail trim",
     location="PetCo",
     start_date="2024-02-07",
     start_time="12:30:00", #2018-09-01T00:00:00-05:00, YYYY-MM-DDTHH:mm:ss.sssZ
     end_date="2024-02-07",
-    end_time="12:30:00",
+    end_time="01:30:00",
     allDay=False,
     description="",
 )
 
-event.owners.append(test_user)
-event.pets.append(pet)
+event1.owners.append(test_user)
+event1.pets.append(pet1)
+event1.pets.append(pet2)
 
-specialist = Specialist(
-    role="Vet",
-    specialist_fname="Carol",
-    specialist_lname="Calista",
-    specialist_email="calistaanimalhospital@gmail.com",
-    specialist_phone="(575) 525-1000",
-    street="1889 Calle de Ninos",
-    street2="Calista",
-    city="Las Cruces",
-    state="NM",
-    zip_code="88005",
-    specialist_comment="I love this vet and the office!! Vet for Lily and Feefee",
+event2 = Event(
+    title="Vacation: Feefee boarded",
+    location="Camp Calista",
+    start_date="2024-02-24",
+    start_time="12:30:00", #2018-09-01T00:00:00-05:00, YYYY-MM-DDTHH:mm:ss.sssZ
+    end_date="2024-02-29",
+    end_time="01:30:00",
+    allDay=False,
+    description="",
 )
 
-pet.specialists.append(specialist)
+event2.owners.append(test_user)
+event2.pets.append(pet1)
+event2.pets.append(pet2)
 
-model.db.session.add(pet)
+# specialist = Specialist(
+#     role="Vet",
+#     specialist_fname="Carol",
+#     specialist_lname="Calista",
+#     specialist_email="calistaanimalhospital@gmail.com",
+#     specialist_phone="(575) 525-1000",
+#     street="1889 Calle de Ninos",
+#     street2="Calista",
+#     city="Las Cruces",
+#     state="NM",
+#     zip_code="88005",
+#     specialist_comment="I love this vet and the office!! Vet for Lily and Feefee",
+# )
+
+# pet.specialists.append(specialist)
+
+model.db.session.add(pet1)
+model.db.session.add(pet2)
 model.db.session.commit()
 
 
